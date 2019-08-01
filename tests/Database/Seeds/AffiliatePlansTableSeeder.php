9cb7308d5a12d0d11493e52b7aec3c6662d06e2e
Keith Brink <keith.brink@gmail.com>
2019-08-01 16:27:47 +0300

    Case sensitivity?

---
+++ tests/Database/Seeds/AffiliatePlansTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace KeithBrink\AffiliatesSpark\Tests\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use KeithBrink\AffiliatesSpark\Models\AffiliatePlan;
+
+class AffiliatePlansTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        factory(AffiliatePlan::class, 3)->create();
+    }
+}
---
+++ tests/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace KeithBrink\AffiliatesSpark\Tests\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(AffiliatePlansTableSeeder::class);
+        $this->call(AffiliatesTableSeeder::class);
+    }
+}

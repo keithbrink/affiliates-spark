4271af0b5692b3f891787cb22204e7461c6b84b6
Keith Brink <keith.brink@gmail.com>
2020-01-02 23:23:22 -0500

    Update cashier config for tests

--- tests/ControllersTest.php
+++ tests/ControllersTest.php
@@ -2,32 +2,30 @@
 
 namespace KeithBrink\AffiliatesSpark\Tests;
 
-use Laravel\Cashier\Cashier;
-
 class ControllersTest extends TestCase
 {
     public function testViewAffiliates()
     {
-        Cashier::useCurrency('usd', '$');
+        config(['cashier.currency' => 'usd']);
 
         $response = $this->actingAs($this->affiliate_user)->get('/affiliates');
         $response->assertStatus(200);
 
-        $currency_string = Cashier::usesCurrencySymbol().number_format($this->affiliate->commissionAmount()->value(), 2);
+        $currency_string = '$'.number_format($this->affiliate->commissionAmount()->value(), 2);
 
         $response->assertSeeText('You receive a '.$currency_string.' commission');
-        $response->assertSee(Cashier::usesCurrencySymbol());
+        $response->assertSee('$');
         $response->assertDontSee('£');
 
-        Cashier::useCurrency('gbp', '£');
+        config(['cashier.currency' => 'gbp']);
 
         $response = $this->actingAs($this->affiliate_user)->get('/affiliates');
         $response->assertStatus(200);
 
-        $currency_string = Cashier::usesCurrencySymbol().number_format($this->affiliate->commissionAmount()->value(), 2);
+        $currency_string = '£'.number_format($this->affiliate->commissionAmount()->value(), 2);
 
         $response->assertSeeText('You receive a '.$currency_string.' commission');
-        $response->assertSee(Cashier::usesCurrencySymbol());
+        $response->assertSee('£');
         $response->assertDontSee('$');
     }
 
@@ -35,20 +33,20 @@ class ControllersTest extends TestCase
     {
         $this->addAffiliateTransaction();
 
-        Cashier::useCurrency('usd', '$');
+        config(['cashier.currency' => 'usd']);
 
         $response = $this->actingAs($this->affiliate_user)->get('/affiliates/transactions');
         $response->assertStatus(200);
 
-        $response->assertSee(Cashier::usesCurrencySymbol());
+        $response->assertSee('$');
         $response->assertDontSee('£');
 
-        Cashier::useCurrency('gbp', '£');
+        config(['cashier.currency' => 'gbp']);
 
         $response = $this->actingAs($this->affiliate_user)->get('/affiliates/transactions');
         $response->assertStatus(200);
 
-        $response->assertSee(Cashier::usesCurrencySymbol());
+        $response->assertSee('£');
         $response->assertDontSee('$');
     }
 
@@ -56,20 +54,20 @@ class ControllersTest extends TestCase
     {
         $this->addAffiliateTransaction();
 
-        Cashier::useCurrency('usd', '$');
+        config(['cashier.currency' => 'usd']);
 
         $response = $this->actingAs($this->affiliate_user)->get('/affiliates/withdraw');
         $response->assertStatus(200);
 
-        $response->assertSee(Cashier::usesCurrencySymbol());
+        $response->assertSee('$');
         $response->assertDontSee('£');
 
-        Cashier::useCurrency('gbp', '£');
+        config(['cashier.currency' => 'gbp']);
 
         $response = $this->actingAs($this->affiliate_user)->get('/affiliates/withdraw');
         $response->assertStatus(200);
 
-        $response->assertSee(Cashier::usesCurrencySymbol());
+        $response->assertSee('£');
         $response->assertDontSee('$');
     }
 }

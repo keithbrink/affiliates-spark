8682d8451a229c73e2daa7e3b9af41f126868d1d
Keith Brink <keith.brink@gmail.com>
2020-04-12 09:52:22 +0300

    Fix affiliate transaction factory

--- database/factories/AffiliateTransactionFactory.php
+++ database/factories/AffiliateTransactionFactory.php
@@ -1,14 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 use KeithBrink\AffiliatesSpark\Models\Affiliate;
 use KeithBrink\AffiliatesSpark\Models\AffiliateTransaction;
 
 $factory->define(AffiliateTransaction::class, function (Faker $faker) {
     return [
         'affiliate_id' => $faker->randomElement(Affiliate::all()->pluck('id')->toArray()),
-        'transaction_id' => Str::random(10),
+        'invoice_id' => rand(1, 99),
         'transaction_date' => $faker->dateTime(),
         'type' => $faker->randomElement(['referral_payment', 'withdrawal']),
         'amount' => $faker->randomFloat(2, 0.1, 100),
